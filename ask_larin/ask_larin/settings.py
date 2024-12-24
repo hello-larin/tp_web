f3b1921de68ded613db62fc79102d77c562c86e7
hello-larin <larinalesha@mail.ru>
2024-12-24 20:47:35 +0300

    gnuunicorn пока не выходит

--- ask_larin/ask_larin/settings.py
+++ ask_larin/ask_larin/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+#STATIC_ROOT = BASE_DIR / 'static'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
@@ -134,4 +135,4 @@ STATICFILES_DIRS = [
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL = '/uploads/'
-MEDIA_ROOT = os.path.join(BASE_DIR, 'uploads/')
+MEDIA_ROOT = BASE_DIR / 'uploads'
